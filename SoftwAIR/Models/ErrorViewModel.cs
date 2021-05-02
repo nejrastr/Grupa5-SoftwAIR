fd0e966019544700bf82e4dfe533ee0f1184d4e5
Hamza Begic <34475478+bespa8@users.noreply.github.com>
2021-05-03 01:23:44 +0200

    Add files via upload

---
+++ SoftwAIR/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SoftwAIR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
