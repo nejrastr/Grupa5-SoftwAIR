2cb9bd97da7f3ba55615650c6664f79c75ae1eee
Hamza Begic <34475478+bespa8@users.noreply.github.com>
2021-05-10 12:23:36 +0200

    Add files via upload
    Pocetne klase za modele

---
+++ SoftwAIR/Models/Kupovina.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftwAIR.Models
+{
+    public abstract class Kupovina
+    {
+        public String statusKupovine;
+        public int idKupovine;
+        public DateTime datumKupovine;
+        public int idKupca;
+
+        public abstract void postaviStatus(String status);
+        public abstract void odustani();
+    }
+}
