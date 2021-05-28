f510573b435858185f11669237763ce26f67a62c
Hamza Begic <34475478+bespa8@users.noreply.github.com>
2021-05-28 19:24:28 +0200

    Add files via upload
    Updateovan model na ASP.NET

---
+++ SoftwAIR-Alpha/Models/Klasa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SoftwAIR.Models
+{
+    public class Klasa
+    {
+        public enum KlasaEnum
+        {
+            [Display(Name = "Biznis")]
+            BIZNIS,
+            [Display(Name = "Prva")]
+            PRVA,
+            [Display(Name = "Ekonomska")]
+            EKONOMSKA
+        }
+
+        public KlasaEnum tipKlase { get; set; }
+
+        public Klasa() { }
+    }
+}
---
+++ SoftwAIR-Alpha/Models/Kupovina.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftwAIR.Models
+{
+    public abstract class Kupovina
+    {
+        #region Properties
+        [Required]
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public String statusKupovine { get; set; }
+        [Required]
+        public DateTime datumKupovine { get; set; }
+        [Required]
+        public int idKupca { get; set; }
+        #endregion
+
+        #region Constructor
+        public Kupovina(int ID, String statusKupovine, DateTime datumKupovine, int idKupca)
+        {
+            this.ID = ID;
+            this.statusKupovine = statusKupovine;
+            this.datumKupovine = datumKupovine;
+            this.idKupca = idKupca;
+        }
+        #endregion
+
+        #region Methods
+        public void postaviStatus(String status) {
+            this.statusKupovine = status;
+        }
+        public void odustani() {
+        //TODO
+        }
+        #endregion
+    }
+}
